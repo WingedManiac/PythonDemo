7cf81d3864c22e365c0b8c1c340c2392975ee5e5
WingedManiac <159825481+WingedManiac@users.noreply.github.com>
2025-12-29 08:20:26 -0600

    type conversion practice

--- PythonDemo.py
+++ PythonDemo.py
@@ -1,4 +1,3 @@
-from datetime import  datetime
-birth_year = input("Enter your birth year: ")
-age = datetime.today().year - int(birth_year)
-print(age)
+weight_lbs = input("Enter Weight (lbs): ")
+weight_kgs = (int(weight_lbs)/2.205)
+print('You weight ' + str(weight_kgs) + 'kgs')
